630e49d352dd4b28c07f450df110b34441f3375d
Mantas Savaniakas <mantasavas@gmail.com>
2018-02-24 13:08:50 +0200

    Added option to change between minute and hour conversion

--- first_homework.m
+++ first_homework.m
@@ -16,17 +16,12 @@ display_candles(mtsla_data.high, mtsla_data.low, mtsla_data.close, mtsla_data.op
 %% reading <<tick>> data from file
 ttsla_data = read_file('tick_bar_tesla.csv', 'MM/dd/yyyy HH:mm:ss');
 
-findgroups(ttsla_data.date);
-
 %% plotting <<tick>> graph
-
 display_candles(ttsla_data.high, ttsla_data.low, ttsla_data.close, ttsla_data.open, 'r', ttsla_data.laikas, 'TSLA Tic 100 Candles Tesla Inc');
 
 
 %% generating random data
-
 random_data = generate_random('17-Feb-2018 00:00:00','17-Feb-2018 23:00:00');
-
 display_candles(random_data.high, random_data.low, random_data.close, random_data.open, 'b', random_data.date', 'Random Data Generated');
 
 
@@ -35,36 +30,12 @@ biggest_gap(10);
 
 
 %% calculate hour or minute bars
-
-%meanBMIfcn = @(p)max(p);
-%BMI = splitapply(meanBMIfcn, p, G)
-
-mtsla_data = [];
-ttsla_data = read_file('tick_bar_tesla.csv', 'MM/dd/yyyy HH:mm:ss');
-
-ttsla_data.time;
-time_split = regexp(ttsla_data.time, ':', 'split');
-D = vertcat(time_split{:});
-
-hours = D(:, 1);
-minutes = D(:, 2);
+mtsla_data = tick_data_conversion('tick_bar_tesla.csv', 'minute');
 
 
+mtsla_data.datetime
 
-
-func = @(date, hour, minute, open, close, high, low) {date{1}, hour{1}, minute{1}, open(1), close(end), max(high), min(low)};
-[G,smokers] = findgroups(ttsla_data.datesimple, D(:, 1), D(:, 2));
-varBP = splitapply(func, ttsla_data.datesimple, hours, minutes, ttsla_data.open, ttsla_data.close, ttsla_data.high, ttsla_data.low, G);
-
-
-date_time = strcat(varBP(:, 1), {' '}, varBP(:, 2), {':'}, varBP(:, 3), {':00'})
-mtsla_data.datetime = datetime(date_time, 'InputFormat', 'MM/dd/yyyy HH:mm:ss');
-
-mtsla_data.open = cell2mat(varBP(:, 4));
-mtsla_data.close = cell2mat(varBP(:, 5));
-mtsla_data.high = cell2mat(varBP(:, 6));
-mtsla_data.low = cell2mat(varBP(:, 7));
-
-display_candles(mtsla_data.high, mtsla_data.low, mtsla_data.close, mtsla_data.open, 'r', mtsla_data.datetime, 'TSLA Minute Tesla Inc');
-
-
+% If generated any hour or minute data, if true display graph
+if ~isempty(mtsla_data)
+    display_candles(mtsla_data.high, mtsla_data.low, mtsla_data.close, mtsla_data.open, 'r', mtsla_data.datetime, 'TSLA Minute Tesla Inc');
+end
